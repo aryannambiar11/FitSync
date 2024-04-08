0718450d04167d76fa41207739a54d5cd9daaac4
aryan.nambiar <aryansnr7@gmail.com>
2024-04-08 15:56:40 -0600

    fixed load template popup scroll functionality for viewing multiple templates

--- src/components/LoadTemplatePopup.js
+++ src/components/LoadTemplatePopup.js
@@ -4,18 +4,6 @@ const LoadTemplatePopup = ({ onClose, onTemplateLoad }) => {
   const savedTemplatesJSON = sessionStorage.getItem("templateData");
   const savedTemplates = savedTemplatesJSON ? JSON.parse(savedTemplatesJSON) : [];
 
-  console.log("savedTemplates:", savedTemplates); // Log savedTemplates for debugging
-  // Check if savedTemplates is not an array or is empty
-  if (!Array.isArray(savedTemplates) || savedTemplates.length === 0) {
-    alert("No saved templates found.");
-    onClose();
-    return null; // Return null if there are no saved templates
-    
-  }
-
-  // Maximum number of templates that can fit within the popup without scrolling
-  const maxTemplatesWithoutScroll = savedTemplates.length;
-
 
   // Function to handle loading a template
   const handleLoadTemplate = (templateName) => {
@@ -41,29 +29,32 @@ const LoadTemplatePopup = ({ onClose, onTemplateLoad }) => {
         <div className="absolute top-[0px] left-[0px] w-[258px] h-[382px] max-w-full max-h-full overflow-auto">
           <div className="absolute top-[0px] left-[0px] rounded-3xs bg-black box-border w-[258px] h-[382px] border-[1px] border-solid border-colors-neutral-white">
             {/* Title */}
-            <div className="absolute top-[54px] left-[32px] inline-block w-[194px] h-[38px]">
+            <div className="absolute top-[36px] left-[32px] inline-block w-[194px] h-[38px] bg-black z-10">
               Load Workout Template:
             </div>
             {/* Templates container with scrolling */}
-            <div className="overflow-y-auto h-[calc(100% - 54px - 38px)] pt-[54px] pb-[38px]">
-              {/* Render buttons for each saved template */}
-              {savedTemplates.slice(0, maxTemplatesWithoutScroll).map((template, index) => (
-                <button
-                  key={template.name}
-                  className="cursor-pointer [border:none] p-0 bg-[transparent] absolute top-[157px] left-[30px] w-[197px] h-[42.2px]"
-                  style={{ top: `${157 + index * 50}px` }} // Adjust top position dynamically
-                  onClick={() => handleLoadTemplate(template.name)}
-                >
-                  <img
-                    className="absolute top-[0px] left-[0px] rounded-xl w-[197px] h-[42.2px]"
-                    alt=""
-                    src="/rectangle-792.svg"
-                  />
-                  <b className="absolute top-[11.1px] left-[3.4px] text-base inline-block font-inter text-colors-neutral-white text-center w-[191.5px] h-[22.1px]">
-                    {template.name}
-                  </b>
-                </button>
-              ))}
+            <div className="absolute top-[16px] left-[0px] right-[0px] w-[230px] h-[300px] overflow-y-auto mt-[20px] z-0">
+
+
+{/* Render buttons for each saved template */}
+{savedTemplates.map((template, index) => (
+  <button
+    key={template.name}
+    className="cursor-pointer [border:none] p-0 bg-[transparent] absolute top-[157px] left-[30px] w-[197px] h-[42.2px]"
+    style={{ top: `${57 + index * 50}px` }} // Adjust top position dynamically
+    onClick={() => handleLoadTemplate(template.name)}
+  >
+    <img
+      className="absolute top-[0px] left-[0px] rounded-xl w-[197px] h-[42.2px]"
+      alt=""
+      src="/rectangle-792.svg"
+    />
+    <b className="absolute top-[11.1px] left-[3.4px] text-base inline-block font-inter text-colors-neutral-white text-center w-[191.5px] h-[22.1px]">
+      {template.name}
+    </b>
+  </button>
+))}
+
             </div>
           </div>
           {/* X icon for closing the popup */}
